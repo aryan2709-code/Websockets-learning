745560ce8b09e7a5016c2c0e694522f09c2debe3
Aryan <bhardwaj7a@gmail.com>
2025-03-01 11:56:21 +0530

    Write the logic for a simple ping pong application

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ const wss = new WebSocketServer({port:8080});
  //event handler 
  wss.on("connection" , function(socket) {
     console.log("User Connection")
-    setInterval(() => {
-        socket.send("Current price of solana is : " + Math.random())
-    }, 5000)
-
+    
     socket.on("message" , (e) => {
-        console.log(e.toString());
+        if(e.toString() === "ping")
+        {
+            socket.send("pong");
+        }
     })
  })
